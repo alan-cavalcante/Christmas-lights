d7c49b7be7a99a00a7e40be50501124e1aebd776
alan-cavalcante <aocaval@gmail.com>
2021-06-29 13:06:28 -0300

    add & remove bars

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
 let luzes = document.getElementsByClassName('luzes')
 let temporizador = ms => new Promise(res => setTimeout(res, ms))
+let ativo = 0
 
-async function cores() {
 
-    for (let i = 0; i < luzes.length; i++) {
-        luzes[i].style.backgroundColor = "green"
-        await temporizador(1000)
+/*Inserir e Remover fileira de luzes*/
+function inserirLinhas(qtd) {
+    let containerMaiorLuzes = document.getElementById("containerMaiorLuzes")
+    let luzinhas = document.getElementById("container-luzes")
+    let novaLuz = luzinhas.cloneNode(true)
+    for (let i = 0; i <= qtd; i++) {
+        if (containerMaiorLuzes.childElementCount > qtd) {
+            containerMaiorLuzes.querySelector("#container-luzes").remove()
+        } else if (containerMaiorLuzes.childElementCount < qtd) {
+            containerMaiorLuzes.appendChild(novaLuz)
+        }
     }
-
-
-    // setInterval(() => {
-    // for (luz of luzes) {
-    //         luz.style.backgroundColor = "green"
-    //     }
-    // }, 1000);
-    console.log(luzes)
 }
+
+/*Iniciar e parar o escalonamento das bolinhas*/
+    async function cores() {
+        if (ativo == 0) {
+            let slider = document.getElementById('slider').value
+            for (let i = 0; i < luzes.length; i++) {
+                luzes[i].style.transform = "scale(2,2)"
+                await temporizador(slider)
+                luzes[i].style.transform = "scale(1,1)"
+            }
+        }
+    }
